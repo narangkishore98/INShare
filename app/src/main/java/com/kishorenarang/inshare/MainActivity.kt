aabb707dcdc0158866beea4f44596b98431556d7
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-07-09 12:45:56 -0400

    [Tarlochan5268] test commit

--- app/src/main/java/com/kishorenarang/inshare/MainActivity.kt
+++ app/src/main/java/com/kishorenarang/inshare/MainActivity.kt
@@ -58,4 +58,5 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
 }
