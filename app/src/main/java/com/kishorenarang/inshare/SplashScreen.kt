28d85cae28b4cb412e3ed341887206ca198b7783
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-07-09 22:01:36 -0400

    [Tarlochan5268] SplashScreen.kt file created and code made for splash screen

---
+++ app/src/main/java/com/kishorenarang/inshare/SplashScreen.kt
@@ -0,0 +1,22 @@
+package com.kishorenarang.inshare
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_splash_screen)
+
+        //3second splash time
+        Handler().postDelayed({
+            //start main activity
+            startActivity(Intent(this, IntroActivity::class.java))
+            //finish this activity
+            finish()
+        },3000)
+    }
+}
