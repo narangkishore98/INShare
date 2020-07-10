eb89862adfc8505553c92583b0499c774ee8fdbb
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-07-10 02:17:48 -0400

    [Tarlochan5268] Added NewsFeedAdapter.kt Class which is Custom Adapter for News Feed

---
+++ app/src/main/java/com/kishorenarang/inshare/Adapter/NewsFeedAdapter.kt
@@ -0,0 +1,34 @@
+package com.kishorenarang.inshare.Adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.kishorenarang.inshare.R
+import kotlinx.android.synthetic.main.item_news_card.view.*
+
+class NewsFeedAdapter(private val newsList:List<NewsItem>) : RecyclerView.Adapter<NewsFeedAdapter.NewsviewHolder>(){
+
+    class NewsviewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    {
+        val txtnewsHeading: TextView = itemView.findViewById(R.id.txtnewsHeading)
+        val txtnewsSubHeading: TextView = itemView.findViewById(R.id.txtnewsSubHeading)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsviewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_news_card,parent,false)
+        return NewsviewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return newsList.size
+    }
+
+    override fun onBindViewHolder(holder: NewsviewHolder, position: Int) {
+        val currentNewsItem = newsList[position]
+        holder.txtnewsHeading.text = currentNewsItem.NewsHeading
+        holder.txtnewsSubHeading.text = currentNewsItem.NewsSubHeading
+    }
+
+}
